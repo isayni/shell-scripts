15e06021993a548311225dd326b50c8adba41eff
Kikuchiyooo <lennysep@protonmail.com>
2021-10-18 16:50:29 +0200

    init

---
+++ hotplug.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# script run by monitor udev rules.
+# manage connected/disconneted HDMI monitor to
+# mirror the primary screen
+#
+export DISPLAY=:0
+export XAUTHORITY=/home/lenny/.Xauthority
+
+function connect {
+    # DisplayPort-0 - default monitor
+    xrandr --output $1 --same-as "DisplayPort-0" --mode 1920x1080
+}
+function disconnect {
+    xrandr --output $1 --off
+}
+function main {
+    # Ports to check
+    for disp in 'HDMI-A-0' 'HDMI-A-1' 'HDMI-A-2'; do
+        xrandr --query | grep "$disp connected" &> /dev/null && connect $disp || disconnect $disp
+    done
+}
+
+main &
