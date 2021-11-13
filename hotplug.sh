41b38d746ba6139eef6a0b31378660ed61ebd95a
Kikuchiyooo <lennysep@protonmail.com>
2021-11-13 01:09:56 +0100

    minor update

--- hotplug.sh
+++ hotplug.sh
@@ -7,14 +7,14 @@
 export DISPLAY=:0
 export XAUTHORITY=/home/lenny/.Xauthority
 
-function connect {
+connect() {
     # DisplayPort-0 - default monitor
     xrandr --output $1 --same-as "DisplayPort-0" --mode 1920x1080
 }
-function disconnect {
+disconnect() {
     xrandr --output $1 --off
 }
-function main {
+main() {
     # Ports to check
     for disp in 'HDMI-A-0' 'HDMI-A-1' 'HDMI-A-2'; do
         xrandr --query | grep "$disp connected" &> /dev/null && connect $disp || disconnect $disp
